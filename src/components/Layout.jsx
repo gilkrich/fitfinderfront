2ee5732190f53492b4b267ad67e051e983a2ed26
guy tsabari <guytsabari123@gmail.com>
2023-07-24 16:06:59 +0300

    HomePage

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Outlet, Link, useParams, } from 'react-router-dom'
+import '../components/Layout.css'
 
 const Layout = () => {
   return (
     <div>
-      <nav>
+      <nav className='top-nav'>
         <Link to='/'>Login</Link>
         <Link to='/signup'>SignUp</Link>
+        <Link to='/homepage'>Home</Link>
       </nav>
+      <Outlet/>
     </div>
   )
 
