3ae264dab54fd5450488b074358c922f1614d379
gilkrich <gildebil2@gmail.com>
2023-07-26 10:06:10 +0300

    layout

--- src/components/MainContext.jsx
+++ src/components/MainContext.jsx
@@ -5,23 +5,24 @@ import axios from 'axios'
 export const Context = createContext({})
 
 const MainContext = ({children}) => {
-  const [user,setuser] = useState({})
+  const [user,setuser] = useState('')
     
   useEffect(()=>{
   async function getuser() { 
       if (localStorage.getItem('token')) {
-        const data = await axios.post("http://localhost:3003/users/istoken", { token:localStorage.getItem('token')})
-        console.log(data)
+        const finduser = await axios.post("http://localhost:3003/users/istoken", { token:JSON.parse(localStorage.getItem('token'))})
+        setuser(finduser.data)
+      }else{
+        setuser('')
       }
     }
     getuser()
   },[])
 
-console.log(user);
 
   return (
     <div>
-       <Context.Provider value={{}}>
+       <Context.Provider value={{userinfo:user}}>
       {children}
     </Context.Provider>
     </div>
